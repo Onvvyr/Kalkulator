cdbea20cea482f11c9b3e314c2bb25191fc3be6c
Onvyr <nikodem.pilachowski@gmail.com>
2021-10-04 10:43:20 +0200

    Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Power(int a, int b)
+        {
+            int wynik = 1;
+            for (int i = b; i > 0; i--)
+            {
+                wynik = wynik * a;
+            }
+            return wynik;
+        }
+    }
+}
